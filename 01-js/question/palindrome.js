efb5e9d882b5b63df75f06227ad428081c990aaa
Vishist Singh Solanki <vishistsingh16gh@gmail.com>
2024-01-09 22:19:04 +0530

    pallindrome in js

---
+++ 01-js/question/palindrome.js
@@ -0,0 +1,14 @@
+/*
+  Implement a function `isPalindrome` which takes a string as argument and returns true/false as its result.
+  Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
+*/
+
+function isPalindrome(str) {
+    const strLower = str.toLowerCase();
+    const filteredStr = strLower.replace(/[^\w]/g, '');
+    const reversedStr = filteredStr.split('').reverse().join('');
+    return filteredStr === reversedStr;
+  }
+  
+  module.exports = isPalindrome;
+  
