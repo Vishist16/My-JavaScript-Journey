4bcd8fdd2471d7940861ad47e1531612949c3f39
Vishist Singh Solanki <vishistsingh16gh@gmail.com>
2024-01-22 23:24:38 +0530

    Completed asyn-promise of week 1 - 1.5

--- Basic Javascript/promise_notes.js
+++ Basic Javascript/promise_notes.js
@@ -21,7 +21,35 @@
 // a.then(onDone);
 
 var d = new Promise(function(resolve){
-    resolve();
+    setTimeout(function(){
+        resolve("data");
+    },1000);
 });
+function callBack(d){
+    console.log(d);
+};
+// console.log(d);
+// d.then(callBack);
 
-console.log(d);
+function vishistsAsyncfunc(){
+    let p = new Promise(function(resolve){
+        setTimeout(function(){
+            resolve("New hi");
+        },3000);
+    });
+    return p;
+};
+
+// const value = vishistsAsyncfunc();
+// value.then(function(){
+//     console.log("hi there");
+// })
+async function main(){
+    // no callbacks and no .then syntax used
+    // await is only valid in a async function and can't be used without a async func
+    let value = await vishistsAsyncfunc(); // as it will take 3 seconds // await is very imp, as it returns resolved val, else if not used will give promise
+    console.log("New hi 1"); // this will only be printed after the await reads value, then only the thread will move to execute the next value
+    console.log(value);
+}
+main();
+console.log("After Main"); // this line will be printed before main() func
